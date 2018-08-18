169c06505cff2d9308d4fac2af503da28fca2b88
Diego Soto <diego.soto12@hotmail.com>
2018-08-18 13:54:01 -0500

    Alerts and errors topbar buttons

--- web-app/src/components/app/home/error-panel-topbar.vue
+++ web-app/src/components/app/home/error-panel-topbar.vue
@@ -2,19 +2,30 @@
         <div class="text-left bg-theme-color-4 rounded-sm shadow-md py-4 px-4">
             <div class="flex flex-row">
                 <span class="flex-1 w-full text-left text-white text-3xl font-semibold">Errores</span>
+                <div class="inline-block border hover:bg-theme-color-4-dark rounded-sm px-6 py-2 cursor-pointer text-white disabled" @click="goErrors()">
+                        <font-awesome-icon :icon="iconGoToErrors" />
+                        <span class="ml-2 uppercase text-sm">Filtrar Errores</span>
+                </div>
             </div>
+
         </div>
 </template>
 
 <script>
   import RouteUtils from "../../../js/utils/route-utils";
-  import {faMoneyBill} from '@fortawesome/free-solid-svg-icons';
+  import {faFilter} from '@fortawesome/free-solid-svg-icons';
   import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
   const methods = {
+     goErrors() {
+      //RouteUtils.goBills();
+    },
   };
 
   const computed = {
+     iconGoToErrors() {
+      return faFilter;
+    },
   };
 
   export default {
