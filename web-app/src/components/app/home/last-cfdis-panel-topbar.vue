5e1bbf1dd278d6f46cd2f3b41968f4de656f3329
Carlos Hernandez <carlos.hernandez@connectit.com.mx>
2018-08-20 17:31:10 -0500

    Changed text at last-cfdi components

--- web-app/src/components/app/home/last-cfdis-panel-topbar.vue
+++ web-app/src/components/app/home/last-cfdis-panel-topbar.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="text-left bg-theme-color-4 rounded-sm shadow-md py-4 px-4">
             <div class="flex flex-row">
-                <span class="flex-1 w-full text-left text-white text-3xl font-semibold">&Uacute;ltimas Facturas</span>
+                <span class="flex-1 w-full text-left text-white text-3xl font-semibold">&Uacute;ltimos CFDI</span>
                 <div class="inline-block border hover:bg-theme-color-4-dark rounded-sm px-6 py-2 cursor-pointer text-white disabled" @click="goBills()">
                     <font-awesome-icon :icon="iconGoToBills" />
                     <span class="ml-2 uppercase text-sm">Ir a facturas</span>
