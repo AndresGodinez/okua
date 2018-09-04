bf88051f952b236451e72a72f72ea886fdb892ef
Carlos Hernandez <carlos.hernandez@connectit.com.mx>
2018-09-03 20:50:16 -0500

    Added Emitter entity with related classes and tests

---
+++ app/Repositories/EmitterRepository.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 3/09/18
+ * Time: 08:43 PM
+ */
+
+namespace App\Repositories;
+
+
+use Doctrine\ORM\EntityRepository;
+
+
+/**
+ * Class EmitterRepository
+ * @package App\Repositories
+ */
+class EmitterRepository extends EntityRepository
+{
+
+}
---
+++ tests/factories/emitter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 3/09/18
+ * Time: 08:03 PM
+ */
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+/** @var $fm \League\FactoryMuffin\FactoryMuffin */
+
+use League\FactoryMuffin\Faker\Facade as Faker;
+
+
+$fm->define(\App\Entities\Emitter::class)->setDefinitions([
+    'name' => Faker::name(),
+    'rfc' => Faker::regexify('[A-Z]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z\d]{3}'),
+    'email' => Faker::freeEmail(),
+]);
